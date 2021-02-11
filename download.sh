0a121e46efb5920f179c01c738160a14ba64c38e
moqingyan <jianih@seas.upenn.edu>
2021-02-11 12:37:12 -0500

    update bash file

---
+++ download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+mkdir VQAR_all
+cd VQAR_all
+wget https://zenodo.org/record/4535413/files/VQAR_code.zip
+unzip VQAR_code.zip
+mv VQAR_code/VQAR .
+mv VQAR_code/Scallop .
+rm -rf VQAR_code
+rm -rf VQAR_code.zip
+cd VQAR
+wget https://zenodo.org/record/4527767/files/data.zip
+unzip data.zip
+rm -rf data.zip
